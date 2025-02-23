16a42f05af383c11a81d8322fe5b7dd65d6fa95f
Muhammad Hasnat Umar <mhasnat749@gmail.com>
2025-02-23 13:06:34 +0000

    Connect To DataBase

--- api/index.js
+++ api/index.js
@@ -1,4 +1,17 @@
 const express= require('express')
+const mongoose= require('mongoose')
+const dotenv = require('dotenv')
+
+
+// we have to initilize it with config
+dotenv.config()
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("Connected To MongoDB")
+})
+.catch((err)=>{
+    console.log(err)
+})
 
 const app = express()
 
