268d8109e08128980daceb5bdd8b97ba388bac26
Muhammad Hasnat Umar <mhasnat749@gmail.com>
2025-03-03 15:24:49 +0000

    create and add OAuth component

--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,11 +1,13 @@
 const express = require ("express")
 const {signup}= require ("../controllers/auth.controller.js")
 const {signin}= require("../controllers/auth.controller.js")
+const {google}= require ("../controllers/auth.controller.js")
 
 const router = express.Router()
 
 
 router.post("/signup", signup)
 router.post("/signin", signin)
+router.post("/google", google)
 
 module.exports= router
