50aa80acd0afafddb1bc4d762ad63688507708de
Muhammad Hasnat Umar <mhasnat749@gmail.com>
2025-03-03 14:03:52 +0000

    firebase setup

---
+++ client/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: "mern-real-estate-395aa.firebaseapp.com",
+  projectId: "mern-real-estate-395aa",
+  storageBucket: "mern-real-estate-395aa.firebasestorage.app",
+  messagingSenderId: "504588473626",
+  appId: "1:504588473626:web:9b3426c6290b90d74c88a3"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
