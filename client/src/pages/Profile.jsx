8e62cf057d31d5024ea16fc87893ba95bd0d8c3a
Muhammad Hasnat Umar <mhasnat749@gmail.com>
2025-03-06 16:26:18 +0000

    setup firbase for file storage

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,13 +1,18 @@
 import {useSelector} from 'react-redux'
-
+import { useRef, useState } from 'react'
 const Profile = () => {
 
+  const fileRef = useRef(null)
+  const [file, setFile]= useState(undefined)
   const {currentUser}= useSelector(state=>state.user)
+  console.log(file)
+  
   return (
     <div className='p-3 max-w-lg mx-auto'>
       <h1 className="text-3xl font-semibold text-center my-7">Profile</h1>
       <form className='flex flex-col gap-4'>
-          <img  className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' src={currentUser.avatar} alt='Profile'/>
+          <input onChange={(e)=>setFile(e.target.files[0])} type='file' ref={fileRef} hidden accept='image/*'/>
+          <img onClick={()=>fileRef.current.click()} className='rounded-full h-24 w-24 object-cover cursor-pointer self-center mt-2' src={currentUser.avatar} alt='Profile'/>
           <input type='text' placeholder='username' className='border p-3 rounded-lg' id='username'/>
           <input type='email' placeholder='email' className='border p-3 rounded-lg' id='email'/>
           <input type='password' placeholder='password' className='border p-3 rounded-lg' id='password'/>
