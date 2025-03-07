1c333fbc492d9c84e3b3c403e2a5b038f5def4d7
Muhammad Hasnat Umar <mhasnat749@gmail.com>
2025-03-07 13:27:51 +0000

    complete the functionality of file upload

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,11 +1,50 @@
 import {useSelector} from 'react-redux'
+import { useEffect } from 'react'
 import { useRef, useState } from 'react'
+import {getDownloadURL, getStorage, ref, uploadBytesResumable} from 'firebase/storage'
+import { app } from '../../firebase'
 const Profile = () => {
 
   const fileRef = useRef(null)
   const [file, setFile]= useState(undefined)
+  const [filePerc, setFilePerc]= useState(0)
+  const [fileUploadError, setFileUploadError]= useState(false)
+  const [formData, setFormData]= useState({})
+  console.log(formData)
   const {currentUser}= useSelector(state=>state.user)
+  
   console.log(file)
+  console.log(filePerc)
+  console.log(formData)
+  console.log(fileUploadError)
+
+  useEffect(()=>{
+    if (file){
+      handleFileUpload(file)
+    }
+  }, [file])
+
+  const handleFileUpload=(file)=>{  
+      const storage= getStorage(app)
+      const fileName= new Date().getTime() + file.name
+      const storageRef= ref(storage, fileName)
+      const uploadTask= uploadBytesResumable(storageRef, file)
+    
+
+
+      uploadTask.on('state_changed', (snapshot)=>{
+            const progress= (snapshot.bytesTransferred/snapshot.totalBytes) * 100;
+            setFilePerc(Math.round(progress))
+        },
+        (error)=>{
+            setFileUploadError(true)
+        },
+        ()=>{
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>
+            setFormData({...formData, avatar: downloadURL})
+          )}
+        )
+  }
   
   return (
     <div className='p-3 max-w-lg mx-auto'>
