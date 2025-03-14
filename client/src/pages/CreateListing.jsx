cb54541c865f0c9901a9df09489809ad384457b6
Muhammad Hasnat Umar <mhasnat749@gmail.com>
2025-03-14 13:35:33 +0000

    complete listing page UI

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,9 +1,76 @@
-import React from 'react'
+
 
 export default function CreateListing() {
   return (
-    <div>
-      create listing
-    </div>
+    <main className="p-3 max-w-4xl mx-auto">
+        <h1 className="text-3xl font-semibold text-center my-7"> Create a listing</h1>
+        <form className="flex flex-col sm:flex-row gap-4">
+            <div className="flex flex-col gap-2 flex-1">
+            <input className=" border p-3 rounded-lg" type="text" placeholder="Name" id="name" maxLength='62' minLength='10' required/>
+            <textarea className=" border p-3 rounded-lg" type="text" placeholder="Description" id="description"  required/>
+            <input className=" border p-3 rounded-lg" type="text" placeholder="Address" id="address" required/>
+            <div className="flex gap-6 flex-wrap">
+                <div className="flex gap-2">
+                    <input className="w-5" type="checkbox" id="sale"/>
+                    <span>Sell</span>
+                </div>
+                <div className="flex gap-2">
+                    <input className="w-5" type="checkbox" id="rent"/>
+                    <span>Rent</span>
+                </div>
+                <div className="flex gap-2">
+                    <input className="w-5" type="checkbox" id="parking"/>
+                    <span>Parking Spot</span>
+                </div>
+                <div className="flex gap-2">
+                    <input className="w-5" type="checkbox" id="furnished"/>
+                    <span>Furnished</span>
+                </div>
+                <div className="flex gap-2">
+                    <input className="w-5" type="checkbox" id="offer"/>
+                    <span>Offer</span>
+                </div>
+            </div>
+            <div className="flex flex-wrap gap-6">
+                <div className="flex items-center gap-2">
+                    <input className="p-3 border border-gray-300 rounded-lg" type="number" min="1" max="10" id="bedrooms"/>
+                    <p>Beds</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <input className="p-3 border border-gray-300 rounded-lg" type="number" min="1" max="10" id="bathroom"/>
+                    <p>Baths</p>
+                </div>
+                <div className="flex items-center gap-2">
+                    <input className="p-3 border border-gray-300 rounded-lg" type="number" min="1" max="10" id="regularPrice"/>
+                    <div className="flex flex-col items-center">
+                    <p>Regular Price</p>
+                    <span className="text-sm">($/month)</span>
+                    </div>
+                   
+                </div>
+                <div className="flex items-center gap-2">
+                    <input className="p-3 border border-gray-300 rounded-lg" type="number" min="1" max="10" id="discountPrice"/>
+                    <div className="flex flex-col items-center">
+                    <p>Discount Price</p>
+                    <span className="text-sm">($/month)</span>
+                    </div>
+                    
+                </div>
+            </div>
+            </div>
+
+            <div className="flex flex-col flex-1 gap-4">
+                <p className="font-semibold">Images:
+                <span className="text-gray-600 ml-2">The first image will be the cover (max-2)</span>
+                </p>
+                <div className="flex gap-4">
+                    <input className="p-3 border border-gray-300 rounded-lg w-full" type="file" id="images" accept="image/*" multiple />
+                    <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opcatity-80">Upload</button>
+                </div>
+                <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Create Listing</button>
+            </div>
+           
+        </form>
+    </main>
   )
 }
