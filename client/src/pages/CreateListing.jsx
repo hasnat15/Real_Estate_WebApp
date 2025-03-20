8d163dd4b44fb4734ab6c16d24a91ff148b6ed55
Muhammad Hasnat Umar <mhasnat749@gmail.com>
2025-03-20 10:57:25 +0000

    enhance image upload functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,9 +1,41 @@
+import { getDownloadURL, getStorage, uploadBytesResumable } from "firebase/storage"
 import { useState } from "react"
+import { app } from "../../firebase"
 
 
 export default function CreateListing() {
     const [files, setFiles]= useState([])
     console.log(files)
+
+    const handleImageSubmit=(e)=>{
+
+        if(files.length>0 && files.length<7){
+            const promises=[]
+
+            for(let i=0; i<files.length; i++ ){
+                promises.push(storeImage(files[i]))
+            }
+        }
+    }
+
+    const storeImage=async (file)=>{
+        return new Promise((resolve, reject)=>{
+            const storage = getStorage(app)
+            const fileName= new Date().getTime()+file.name
+            const storageRef= ref(storage, fileName)
+            const uploadTask= uploadBytesResumable(storageRef, file)
+            uploadTask.on("state changed",
+                 (error)=>{
+                    reject(error)
+                 },
+                ()=>{
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL)=>{
+                        resolve(downloadURL)
+                    })
+                }
+                )
+        })
+    }
   return (
     <main className="p-3 max-w-4xl mx-auto">
         <h1 className="text-3xl font-semibold text-center my-7"> Create a listing</h1>
@@ -70,7 +102,7 @@ export default function CreateListing() {
                     <input 
                     onChange={(e)=>setFiles(e.target.files)}
                     className="p-3 border border-gray-300 rounded-lg w-full" type="file" id="images" accept="image/*" multiple />
-                    <button className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opcatity-80">Upload</button>
+                    <button type="button" onClick={handleImageSubmit} className="p-3 text-green-700 border border-green-700 rounded uppercase hover:shadow-lg disabled:opcatity-80">Upload</button>
                 </div>
                 <button className="p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80">Create Listing</button>
             </div>
